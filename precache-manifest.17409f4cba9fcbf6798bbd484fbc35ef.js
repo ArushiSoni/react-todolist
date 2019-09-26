0ffd878d6d9cc134ce03355d458672f9d2e44a68
Arushi Soni <arushisoni01@gmail.com>
2019-09-26 17:54:15 +0530

    Updates

---
+++ precache-manifest.17409f4cba9fcbf6798bbd484fbc35ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca6bc45ec88bf119bae93ca34003d722",
+    "url": "/react-todolist/index.html"
+  },
+  {
+    "revision": "85a0f0790e53afa6fa9c",
+    "url": "/react-todolist/static/css/main.d5e57afc.chunk.css"
+  },
+  {
+    "revision": "026191db50aee3f97689",
+    "url": "/react-todolist/static/js/2.2d78d83c.chunk.js"
+  },
+  {
+    "revision": "85a0f0790e53afa6fa9c",
+    "url": "/react-todolist/static/js/main.8eea3e25.chunk.js"
+  },
+  {
+    "revision": "9001a00a14ccf68a184d",
+    "url": "/react-todolist/static/js/runtime-main.0b67595e.js"
+  }
+]);
